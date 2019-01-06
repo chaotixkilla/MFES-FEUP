bd3afa9bc02244f99ec1191f3baced39a66b736b
Sérgio Salgado <sergiodias.17@hotmail.com>
2019-01-06 19:00:28 +0000

    missing 20% of the interface

--- generated/java/src/client/Client.java
+++ generated/java/src/client/Client.java
@@ -206,26 +206,72 @@ public class Client {
 	}
 	
 	private void manageRepositoryMenu() {
-		/*VDMSet clonableReps = this.github.getClonableRepositories(this.github.getUser(this.github.getLoggedInUsername()));
-		int index = 1;
 		int option = -1;
-		HashMap<Integer, String> repMapping = new HashMap<Integer, String>();
 		
 		this.printGithubLogo();
 		System.out.println("||                                      ||");
-		System.out.println("|| Choose a repository to clone:        ||");
+		System.out.println("|| 1. Navigate your owned repositories. ||");
+		System.out.println("|| 2. Navigate repositories you         ||");
+		System.out.println("||    collaborate to.                   ||");
 		System.out.println("||                                      ||");
+		System.out.println("|| 0. Back.                             ||");
+		System.out.println("||                                      ||");
+		System.out.println("|| Choose an option.                    ||");
+		System.out.println("||                                      ||");
+		System.out.println("||||||||||||||||||||||||||||||||||||||||||");
 		
-		for(Iterator<Repository> i = clonableReps.iterator(); i.hasNext();) {
-			Repository rep = i.next();
-			System.out.println("|| " + index + ". " + rep.getName());
-			
-			repMapping.put(index, rep.getName());
-			index++;
+		while(option < 0 || option > 2) {
+			String optionString = this.scanner.nextLine();
+			try {
+				option = Integer.parseInt(optionString);
+			} catch(NumberFormatException e) {
+				System.out.println("Invalid input. Try again.");
+			}
+		}
+		
+		switch(option) {
+			case 1:
+				this.ownedRepositoriesMenu();
+				break;
+			case 2:
+				this.collabRepositoriesMenu();
+				break;
+			case 0:
+				this.printMainMenu();
+				break;
+			default:
+				System.out.println("Invalid input. Try again.");
+				break;
+		}
+	}
+	
+	private void ownedRepositoriesMenu() {
+		VDMSet ownedRepositories = this.github.getAllOwnedRepositoriesName(this.github.getUser(this.github.getLoggedInUsername()));
+		HashMap<Integer, String> repMapping = new HashMap<Integer, String>();
+		int index = 0;
+		int option = -1;
+		
+		this.printGithubLogo();
+		System.out.println("||                                      ||");
+		System.out.println("|| Repositories you own:                ||");
+		System.out.println("||                                      ||");
+
+		if(ownedRepositories.size() > 0) {
+			index = 1;
+			for(Iterator<String> i = ownedRepositories.iterator(); i.hasNext();) {
+				String repName = i.next();
+				Repository rep = this.github.getSpecificOwnedRepository(this.github.getUser(this.github.getLoggedInUsername()), repName);
+				System.out.println("|| " + index + ". " + rep.getName() + " " + rep.getVisibility().toString());
+				
+				repMapping.put(index, rep.getName());
+				index++;
+			}
+		} else {
+			System.out.println("|| You do not own any repositories :(   ||");
 		}
 		
 		System.out.println("||                                      ||");
-		System.out.println("|| 0. Back                              ||");
+		System.out.println("|| 0. Back.                             ||");
 		System.out.println("||                                      ||");
 		System.out.println("|| Choose an option.                    ||");
 		System.out.println("||                                      ||");
@@ -241,10 +287,259 @@ public class Client {
 		}
 		
 		if(option == 0) {
-			this.printMainMenu();
+			this.manageRepositoryMenu();
 		} else {
-			System.out.println("Chosen repository " + repMapping.get(option));
-		}*/
+			System.out.println("Repository " + repMapping.get(option) + " selected.");
+			this.manageOwnedRepositoryMenu(repMapping.get(option));
+		}
+	}
+	
+	/* TODO */
+	private void manageOwnedRepositoryMenu(String repName) {
+		Repository rep = this.github.getSpecificOwnedRepository(this.github.getUser(this.github.getLoggedInUsername()), repName);
+		int option = -1;
+		
+		this.printGithubLogo();
+		System.out.println("||                                      ||");
+		System.out.println("|| Repository " + repName + " " + rep.getVisibility().toString());
+		System.out.println("||                                      ||");
+		System.out.println("||||||||||||||||||||||||||||||||||||||||||");
+		System.out.println("||                                      ||");
+		System.out.println("|| 1. Add a new collaborator.           ||");
+		System.out.println("|| 2. Remove a collaborator.            ||");
+		System.out.println("|| 3. Make repository public.           ||");
+		System.out.println("|| 4. Make repository private.          ||");
+		System.out.println("|| 5. Delete repository.                ||");
+		System.out.println("||                                      ||");
+		System.out.println("|| 0. Back.                             ||");
+		System.out.println("||                                      ||");
+		System.out.println("|| Choose an option.                    ||");
+		System.out.println("||                                      ||");
+		System.out.println("||||||||||||||||||||||||||||||||||||||||||");
+		
+		while(option < 0 || option > 5) {
+			String optionString = this.scanner.nextLine();
+			try {
+				option = Integer.parseInt(optionString);
+			} catch(NumberFormatException e) {
+				System.out.println("Invalid input. Try again.");
+			}
+		}
+		
+		switch(option) {
+			case 1:
+				this.addCollaboratorMenu(repName);
+				break;
+			case 2:
+				this.removeCollaboratorMenu(repName);
+				break;
+			case 3:
+				this.github.makeRepositoryPublic(this.github.getUser(this.github.getLoggedInUsername()), repName);
+				System.out.println("Repository " + repName + " is now public!");
+				this.ownedRepositoriesMenu();
+				break;
+			case 4:
+				this.github.makeRepositoryPrivate(this.github.getUser(this.github.getLoggedInUsername()), repName);
+				System.out.println("Repository " + repName + " is now private!");
+				this.ownedRepositoriesMenu();
+				break;
+			case 5:
+				this.deleteRepositoryMenu(repName);
+				break;
+			case 0:
+				this.ownedRepositoriesMenu();
+				break;
+			default:
+				break;
+		}
+	}
+	
+	private void addCollaboratorMenu(String repName) {
+		Repository rep = this.github.getSpecificOwnedRepository(this.github.getUser(this.github.getLoggedInUsername()), repName);
+		String newCollabName = "-1";
+		
+		this.printGithubLogo();
+		System.out.println("||                                      ||");
+		System.out.println("|| Current collaborators:               ||");
+		
+		if(rep.getCollaborators().size() > 0) {
+			for(Iterator<User> i = rep.getCollaborators().iterator(); i.hasNext();) {
+				User user = i.next();
+				System.out.println("||  " + user.getUsername());
+			}
+		} else {
+			System.out.println("||  There are no collaborators :(       ||");
+		}
+		
+		while(this.github.getUser(newCollabName).getUsername() == null || newCollabName.equals(this.github.getLoggedInUsername())) {
+			System.out.println("||                                      ||");
+			System.out.println("|| Insert the new collaborator username ||");
+			System.out.print("||  ");
+			newCollabName = this.scanner.nextLine();
+			
+			if(this.github.getUser(newCollabName).getUsername() == null) {
+				System.out.println("The user " + newCollabName + " does not exist!");
+			}
+			
+			if(newCollabName.equals(this.github.getLoggedInUsername())) {
+				System.out.println("You cannot add yourself as collaborator!");
+			}
+		}
+		
+		System.out.println("||                                      ||");
+		System.out.println("||||||||||||||||||||||||||||||||||||||||||");
+		
+		this.github.addCollaborator(this.github.getUser(this.github.getLoggedInUsername()), repName, this.github.getUser(newCollabName));
+		System.out.println("User " + newCollabName + " is now a " + repName + " collaborator!");
+		this.manageOwnedRepositoryMenu(repName);
+	}
+	
+	private void removeCollaboratorMenu(String repName) {
+		Repository rep = this.github.getSpecificOwnedRepository(this.github.getUser(this.github.getLoggedInUsername()), repName);
+		int index = 0;
+		int option = -1;
+		HashMap<Integer, String> collabMapping = new HashMap<Integer, String>();
+		
+		if(rep.getCollaborators().size() > 0) {
+			this.printGithubLogo();
+			System.out.println("||                                      ||");
+			System.out.println("|| Current collaborators:               ||");
+			
+			index = 1;
+			for(Iterator<User> i = rep.getCollaborators().iterator(); i.hasNext();) {
+				User user = i.next();
+				System.out.println("||  " + index + ". " + user.getUsername());
+				
+				collabMapping.put(index, user.getUsername());
+				index++;
+			}
+			
+			System.out.println("||                                      ||");
+			System.out.println("|| 0. Back.                             ||");
+			System.out.println("||                                      ||");
+			System.out.println("|| Choose an option.                    ||");
+			System.out.println("||                                      ||");
+			System.out.println("||||||||||||||||||||||||||||||||||||||||||");
+			
+			while(option < 0 || option > index) {
+				String optionString = this.scanner.nextLine();
+				try {
+					option = Integer.parseInt(optionString);
+				} catch(NumberFormatException e) {
+					System.out.println("Invalid input. Try again.");
+				}
+			}
+			
+			if(option == 0) {
+				this.manageOwnedRepositoryMenu(repName);
+			} else {
+				rep.removeCollaborator(this.github.getUser(this.github.getLoggedInUsername()), this.github.getUser(collabMapping.get(option)));
+				System.out.println("User " + collabMapping.get(option) + " is no longer a collaborator of " + repName + "!");
+				this.manageOwnedRepositoryMenu(repName);
+			}
+			
+		} else {
+			System.out.println("You cannot remove collaborators as this repository has none!");
+			this.manageOwnedRepositoryMenu(repName);
+		}
+	}
+	
+	private void deleteRepositoryMenu(String repName) {
+		
+		int option = -1;
+		this.printGithubLogo();
+		System.out.println("||                                      ||");
+		System.out.println("|| Are you sure you want to delete      ||");
+		System.out.println("|| this repository? This operation is   ||");
+		System.out.println("|| permanent!                           ||");
+		System.out.println("||                                      ||");
+		System.out.println("|| 1. YES                               ||");
+		System.out.println("|| 2. NO                                ||");
+		System.out.println("||                                      ||");
+		System.out.println("|| Choose an option.                    ||");
+		System.out.println("||                                      ||");
+		System.out.println("||||||||||||||||||||||||||||||||||||||||||");
+		
+
+		while(option < 1 || option > 2) {
+			String optionString = this.scanner.nextLine();
+			try {
+				option = Integer.parseInt(optionString);
+			} catch(NumberFormatException e) {
+				System.out.println("Invalid input. Try again.");
+			}
+		}
+		
+		switch(option) {
+			case 1: 
+				this.github.deleteRepository(this.github.getUser(this.github.getLoggedInUsername()), repName);
+				System.out.println("Repository " + repName + " was successfully deleted!");
+				this.ownedRepositoriesMenu();
+				break;
+			case 2:
+				this.manageOwnedRepositoryMenu(repName);
+				break;
+			default:
+				System.out.println("Invalid input. Try again.");
+				break;
+		}
+	}
+	
+	private void collabRepositoriesMenu() {
+		VDMSet availableRepositories = this.github.getAllAvailableRepositoriesName(this.github.getUser(this.github.getLoggedInUsername()));
+		HashMap<Integer, String> repMapping = new HashMap<Integer, String>();
+		int index = 0;
+		int option = -1;
+		
+		this.printGithubLogo();
+		System.out.println("||                                      ||");
+		System.out.println("|| Repositories you collaborate to:     ||");
+		System.out.println("||                                      ||");
+
+		if(availableRepositories.size() > 0) {
+			index = 1;
+			for(Iterator<String> i = availableRepositories.iterator(); i.hasNext();) {
+				String repName = i.next();
+				Repository rep = this.github.getSpecificOwnedRepository(this.github.getUser(this.github.getLoggedInUsername()), repName);
+				System.out.println("|| " + index + ". " + rep.getName() + " " + rep.getVisibility().toString());
+				
+				repMapping.put(index, rep.getName());
+				index++;
+			}
+		} else {
+			System.out.println("|| You do not collaborate to any        ||");
+			System.out.println("|| repositories :(                      ||");
+		}
+		
+		System.out.println("||                                      ||");
+		System.out.println("|| 0. Back.                             ||");
+		System.out.println("||                                      ||");
+		System.out.println("|| Choose an option.                    ||");
+		System.out.println("||                                      ||");
+		System.out.println("||||||||||||||||||||||||||||||||||||||||||");
+		
+		while(option < 0 || option > index) {
+			String optionString = this.scanner.nextLine();
+			try {
+				option = Integer.parseInt(optionString);
+			} catch(NumberFormatException e) {
+				System.out.println("Invalid input. Try again.");
+			}
+		}
+		
+		if(option == 0) {
+			this.manageRepositoryMenu();
+		} else {
+			System.out.println("Repository " + repMapping.get(option) + " selected.");
+			this.manageCollabRepositoryMenu(repMapping.get(option));
+		}
+	}
+	
+	/* TODO */
+	private void manageCollabRepositoryMenu(String repName) {
+		Repository rep = this.github.getSpecificAvailableRepository(this.github.getUser(this.github.getLoggedInUsername()), repName);
+		
+		
 	}
 	
 	private void deleteUserMenu() {
--- generated/java/src/model/GithubTest.java
+++ generated/java/src/model/GithubTest.java
@@ -1,5 +1,4 @@
 package model;
-
 import java.util.*;
 import org.overture.codegen.runtime.*;
 
@@ -9,7 +8,9 @@ public class GithubTest {
   private Utilities u = new Utilities();
   private User u1 = new User("user1", "mail1", "password1");
   private User u2 = new User("user2", "mail2", "password2");
+  private User u3 = new User("user3", "mail3", "password3");
   private Repository rep;
+  private Repository rep2;
   private Branch b;
   private Commit c;
 
@@ -18,21 +19,28 @@ public class GithubTest {
     return;
   }
 
-  private void doTests() {
+  private void createUserTest() {
 
     assertTrue(Utils.empty(g.getUsers()));
     g.addUser(u1);
-    assertTrue(SetUtil.inSet("user1", g.getAllUsernames()));
     g.addUser(u2);
+    assertTrue(SetUtil.inSet("user1", g.getAllUsernames()));
     assertTrue(SetUtil.inSet("user2", g.getAllUsernames()));
-    assertTrue(Utils.equals("password1", u1.getPassword()));
-    assertTrue(Utils.equals("password2", u2.getPassword()));
     assertTrue(Utils.equals(u1, g.getUser("user1")));
+    assertTrue(Utils.equals(u2, g.getUser("user2")));
     assertTrue(Utils.equals(SetUtil.set(u1, u2), g.getUsers()));
+  }
+
+  private void loginTest() {
+
     assertTrue(Utils.equals("undef", g.getLoggedInUsername()));
     assertTrue(!(g.login("user1", "wrong password")));
     assertTrue(g.login("user1", "password1"));
     assertTrue(Utils.equals("user1", g.getLoggedInUsername()));
+  }
+
+  private void createRepositoryTest() {
+
     assertTrue(Utils.empty(g.getRepositories()));
     g.createRepository(u1, "repName");
     rep = g.getSpecificOwnedRepository(u1, "repName");
@@ -40,16 +48,49 @@ public class GithubTest {
     assertTrue(Utils.equals(rep, g.getSpecificAvailableRepository(u1, "repName")));
     assertTrue(Utils.equals(SetUtil.set("repName"), g.getAllAvailableRepositoriesName(u1)));
     assertTrue(Utils.equals(u1, rep.getOwner()));
+    assertTrue(Utils.equals(SetUtil.set("master"), rep.getBranchesNames()));
+    assertTrue(rep.isPublicRepository());
+  }
+
+  private void addCollaboratorTest() {
+
+    assertTrue(Utils.empty(rep.getCollaborators()));
     g.addCollaborator(u1, "repName", u2);
     assertTrue(Utils.equals(SetUtil.set(u2), rep.getCollaborators()));
     assertTrue(Utils.equals(SetUtil.set("repName"), g.getAllAvailableRepositoriesName(u2)));
     assertTrue(Utils.equals(SetUtil.set(rep), g.getUserAvailableRepositories(u2)));
-    assertTrue(Utils.equals(SetUtil.set("master"), rep.getBranchesNames()));
+  }
+
+  private void repositoryVisibilityTests() {
+
     assertTrue(rep.isPublicRepository());
     rep.toggleVisibility(u1);
     assertTrue(!(rep.isPublicRepository()));
     rep.toggleVisibility(u1);
     assertTrue(rep.isPublicRepository());
+    rep.makeRepositoryPrivate(u1);
+    assertTrue(Utils.equals(model.quotes.PrivateQuote.getInstance(), rep.getVisibility()));
+    rep.makeRepositoryPublic(u1);
+    assertTrue(Utils.equals(model.quotes.PublicQuote.getInstance(), rep.getVisibility()));
+  }
+
+  private void getClonableRepositoriesTest() {
+
+    g.addUser(u3);
+    g.makeRepositoryPrivate(u1, "repName");
+    assertTrue(Utils.equals(model.quotes.PrivateQuote.getInstance(), rep.getVisibility()));
+    assertTrue(Utils.equals(SetUtil.set(rep), g.getClonableRepositories(u1)));
+    assertTrue(Utils.equals(SetUtil.set(rep), g.getClonableRepositories(u2)));
+    assertTrue(Utils.empty(g.getClonableRepositories(u3)));
+    g.makeRepositoryPublic(u1, "repName");
+    assertTrue(Utils.equals(model.quotes.PublicQuote.getInstance(), rep.getVisibility()));
+    assertTrue(Utils.equals(SetUtil.set(rep), g.getClonableRepositories(u1)));
+    assertTrue(Utils.equals(SetUtil.set(rep), g.getClonableRepositories(u2)));
+    assertTrue(Utils.equals(SetUtil.set(rep), g.getClonableRepositories(u3)));
+  }
+
+  private void createBranchTest() {
+
     g.createBranch(u2, "repName", "branchName");
     assertTrue(Utils.equals(SetUtil.set("master", "branchName"), rep.getBranchesNames()));
     assertTrue(
@@ -59,6 +100,18 @@ public class GithubTest {
     b = rep.getSpecificBranch("branchName");
     assertTrue(Utils.equals(rep, b.getRepository()));
     assertTrue(Utils.empty(b.getCommits()));
+  }
+
+  private void deleteBranchTest() {
+
+    g.createBranch(u2, "repName", "deleteBranchTest");
+    assertTrue(SetUtil.inSet("deleteBranchTest", rep.getBranchesNames()));
+    g.deleteBranch(u2, "repName", "deleteBranchTest");
+    assertTrue(!(SetUtil.inSet("deleteBranchTest", rep.getBranchesNames())));
+  }
+
+  private void createCommitTest() {
+
     g.createCommit(u2, "repName", "branchName", "1commitID", "commitMessage1");
     assertTrue(Utils.equals(1L, b.getCommits().size()));
     c = ((Commit) Utils.get(b.getCommits(), 1L));
@@ -67,15 +120,77 @@ public class GithubTest {
     assertTrue(Utils.equals("commitMessage1", c.getMessage()));
     g.createCommit(u1, "repName", "branchName", "2commitID", "commitMessage2");
     assertTrue(Utils.equals(2L, b.getCommits().size()));
+  }
+
+  private void mergeBranchesTest() {
+
+    g.createBranch(u1, "repName", "newBranchName");
+    g.createCommit(u1, "repName", "newBranchName", "3commitID", "commitMessage3");
+    g.mergeBranches(u1, "repName", "branchName", "newBranchName");
+    assertTrue(!(SetUtil.inSet("newBranchName", rep.getBranchesNames())));
+    assertTrue(Utils.equals(3L, b.getCommits().size()));
+    g.createBranch(u2, "repName", "newBranchName2");
+    g.createCommit(u2, "repName", "newBranchName2", "4commitID", "commitMessage4");
+    g.mergeBranches(u2, "repName", "branchName", "newBranchName2");
+    assertTrue(!(SetUtil.inSet("newBranchName2", rep.getBranchesNames())));
+    assertTrue(Utils.equals(4L, b.getCommits().size()));
+  }
+
+  private void removeCollaboratorTest() {
+
+    assertTrue(Utils.equals(SetUtil.set(u2), rep.getCollaborators()));
     g.removeCollaborator(u1, "repName", u2);
+    assertTrue(Utils.empty(rep.getCollaborators()));
+  }
+
+  private void logoutTest() {
+
+    assertTrue(Utils.equals("user1", g.getLoggedInUsername()));
     g.logout();
     assertTrue(Utils.equals("undef", g.getLoggedInUsername()));
+  }
+
+  private void deleteUserTest() {
+
+    assertTrue(Utils.equals(SetUtil.set(u1, u2, u3), g.getUsers()));
     assertTrue(g.login("user2", "password2"));
     g.deleteUser("user2");
+    assertTrue(Utils.equals("undef", g.getLoggedInUsername()));
+    assertTrue(Utils.equals(SetUtil.set(u1, u3), g.getUsers()));
+    g.createRepository(u3, "repNameD");
+    rep2 = g.getSpecificOwnedRepository(u3, "repNameD");
+    assertTrue(Utils.equals(SetUtil.set(rep, rep2), g.getClonableRepositories(u1)));
+    g.deleteUser("user3");
+    assertTrue(Utils.equals(SetUtil.set(u1), g.getUsers()));
+    assertTrue(Utils.equals(SetUtil.set(rep), g.getClonableRepositories(u1)));
+  }
+
+  private void deleteRepositoryTest() {
+
+    assertTrue(Utils.equals(SetUtil.set(rep), g.getRepositories()));
+    assertTrue(Utils.equals(SetUtil.set(rep), g.getUserAvailableRepositories(u1)));
     assertTrue(g.login("user1", "password1"));
     g.deleteRepository(u1, "repName");
     assertTrue(Utils.empty(g.getRepositories()));
     assertTrue(Utils.empty(g.getUserAvailableRepositories(u1)));
+  }
+
+  private void doTests() {
+
+    createUserTest();
+    loginTest();
+    createRepositoryTest();
+    addCollaboratorTest();
+    repositoryVisibilityTests();
+    getClonableRepositoriesTest();
+    createBranchTest();
+    deleteBranchTest();
+    createCommitTest();
+    mergeBranchesTest();
+    removeCollaboratorTest();
+    logoutTest();
+    deleteUserTest();
+    deleteRepositoryTest();
   }
 
   public static void main() {
@@ -96,8 +211,12 @@ public class GithubTest {
         + Utils.toString(u1)
         + ", u2 := "
         + Utils.toString(u2)
+        + ", u3 := "
+        + Utils.toString(u3)
         + ", rep := "
         + Utils.toString(rep)
+        + ", rep2 := "
+        + Utils.toString(rep2)
         + ", b := "
         + Utils.toString(b)
         + ", c := "
